856f264ab678484e0e2d61783f48b963d2a2e781
IanaZubova <78699873+IanaZubova@users.noreply.github.com>
2021-02-14 20:19:00 +0100

    Delete Lesson 4 directory

--- Lesson 4/5.py
+++
@@ -1,5 +0,0 @@
-from functools import reduce
-
-my_list = [el for el in range(100, 1000+1) if el % 2 == 0]
-
-print(reduce(lambda el_1, el_2: el_1 * el_2, my_list))
--- Lesson 4/6_2.py
+++
@@ -1,8 +0,0 @@
-from itertools import cycle
-
-condition = 0
-for el in cycle("HappyMonday"):
-    if condition > 21:
-        break
-    print(el)
-    condition += 1
